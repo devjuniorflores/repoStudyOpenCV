c45f13298aed7d32830dd9f203eaa7fcc6f73fd4
Jndrs <junior.andres2111@gmail.com>
2025-08-08 23:54:06 -0500

    update files

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ img = cv2.imread("images.jpeg")
 if img is None:
     print("No se pudo cargar la imagen.")
 else:
-    # Invertir horizontalmente (flipCode=1)
+    # Invertir horizontalmente (flipCode=1), Invertir verticalmente (flipCode=0)
     img_invertida = cv2.flip(img, 0)
     cv2.imwrite("copia_invertida_vertical.jpeg", img_invertida)
     print("Imagen invertida guardada como copia_invertida_vertical.jpeg")
